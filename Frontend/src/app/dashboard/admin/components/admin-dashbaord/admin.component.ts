8cdf94534af4645660867517769177ebba516791
amulyamalothu <malothuamulya@gmail.com>
2025-09-04 15:30:34 +0530

    added style url

--- Frontend/src/app/dashboard/admin/components/admin-dashbaord/admin.component.ts
+++ Frontend/src/app/dashboard/admin/components/admin-dashbaord/admin.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin',
-   standalone: false,
-
+  standalone: false,
   templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
   showAdminAlert = true;
